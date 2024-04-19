0906be95dab743e84b2796eca8729aedc8e8858d
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-04-19 17:18:13 +0200

    Update end to end to consider DB

--- end-to-end-test.sh
+++ end-to-end-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-backend_response=$(curl -I -s --show-error http://localhost:4242 | grep "HTTP/1.1")
+backend_response=$(curl -I -s --show-error http://localhost:4242/todos | grep "HTTP/1.1")
 echo $backend_response
 if [[ "$backend_response" == "HTTP/1.1 200 OK"* ]]; then
     echo "Sucessfully connected to backend"
