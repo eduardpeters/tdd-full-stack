b0db73eed2bfb366df7ca837b034c855e6b3648e
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-04-26 19:45:29 +0200

    useTodos returns todos array

--- frontend/src/hooks/useTodos.tsx
+++ frontend/src/hooks/useTodos.tsx
@@ -3,21 +3,23 @@ import { Todo } from '../types.ts';
 import { getAllTodos } from '../services/TodosService.ts';
 
 export default function useTodos() {
-  const [todos, setTodos] = useState<Todo | undefined>(undefined);
-  const [error, setError] = useState<boolean>(true);
+  const [todos, setTodos] = useState<Todo[] | undefined>(undefined);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     async function getTodos() {
       const response = await getAllTodos();
       if ('error' in response) {
         setError(true);
+      } else {
+        setTodos(response);
       }
     }
 
     if (todos === undefined) {
       getTodos();
     }
-  }, []);
+  });
 
   return [todos, error];
 }
--- frontend/test/useTodos.spec.jsx
+++ frontend/test/useTodos.spec.jsx
@@ -1,5 +1,5 @@
 import { afterEach, describe, expect, test, vi } from 'vitest';
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import useTodos from '../src/hooks/useTodos.tsx';
 
 // eslint-disable-next-line no-undef
@@ -15,12 +15,35 @@ describe('useTodos hook', () => {
     global.fetch.mockReset();
   });
 
-  test('An error is returned if there is no response', () => {
+  test('An error is returned if there is no response', async () => {
     fetch.mockResolvedValue(createFetchResponse({}, false));
-    const { result } = renderHook(() => useTodos());
+    const { result } = await waitFor(() => renderHook(() => useTodos()));
     const [todos, error] = result.current;
 
     expect(todos).toBe(undefined);
     expect(error).toBe(true);
+  });
+
+  test('An empty array is returned if there are no todos', async () => {
+    const mockTodos = [];
+    fetch.mockResolvedValue(createFetchResponse(mockTodos));
+    const { result } = await waitFor(() => renderHook(() => useTodos()));
+    const [todos, error] = result.current;
+
+    expect(todos).to.deep.equal([]);
+    expect(error).toBe(false);
+  });
+
+  test('A todos array is returned', async () => {
+    const mockTodos = [
+      { id: 1, description: 'First todo', isComplete: false },
+      { id: 2, description: 'Second todo', isComplete: true },
+    ];
+    fetch.mockResolvedValue(createFetchResponse(mockTodos));
+    const { result } = await waitFor(() => renderHook(() => useTodos()));
+    const [todos, error] = result.current;
+
+    expect(todos).to.deep.equal(mockTodos);
+    expect(error).toBe(false);
   });
 });
