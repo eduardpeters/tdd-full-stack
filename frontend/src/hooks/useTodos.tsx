f523687c30a4a7e06c5983105f247a070868988b
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-04-26 19:23:47 +0200

    Use states in useTodos

--- frontend/src/hooks/useTodos.tsx
+++ frontend/src/hooks/useTodos.tsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from 'react';
 import { Todo } from '../types.ts';
+import { getAllTodos } from '../services/TodosService.ts';
 
 export default function useTodos() {
   const [todos, setTodos] = useState<Todo | undefined>(undefined);
   const [error, setError] = useState<boolean>(true);
 
   useEffect(() => {
-    setError(true);
+    async function getTodos() {
+      const response = await getAllTodos();
+      if ('error' in response) {
+        setError(true);
+      }
+    }
+
+    if (todos === undefined) {
+      getTodos();
+    }
   }, []);
 
   return [todos, error];
--- frontend/test/useTodos.spec.jsx
+++ frontend/test/useTodos.spec.jsx
@@ -1,12 +1,25 @@
-import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import { renderHook } from '@testing-library/react';
 import useTodos from '../src/hooks/useTodos.tsx';
 
+// eslint-disable-next-line no-undef
+global.fetch = vi.fn();
+
+function createFetchResponse(data, isOk = true) {
+  return { json: () => new Promise((resolve) => resolve(data)), ok: isOk };
+}
+
 describe('useTodos hook', () => {
+  afterEach(() => {
+    // eslint-disable-next-line no-undef
+    global.fetch.mockReset();
+  });
+
   test('An error is returned if there is no response', () => {
+    fetch.mockResolvedValue(createFetchResponse({}, false));
     const { result } = renderHook(() => useTodos());
-    console.log(result);
     const [todos, error] = result.current;
+
     expect(todos).toBe(undefined);
     expect(error).toBe(true);
   });
