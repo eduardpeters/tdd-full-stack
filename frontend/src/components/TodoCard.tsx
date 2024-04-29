366df753b6871894afb8490f14378e821ccbd47d
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-04-29 21:44:19 +0200

    Todo completeness can be updated

--- frontend/src/components/TodoCard.tsx
+++ frontend/src/components/TodoCard.tsx
@@ -1,16 +1,34 @@
-import type { Todo } from '../types.ts';
+import { updateTodo } from '../services/TodosService.ts';
+import type { Todo, ResponseError } from '../types.ts';
 
 interface TodoCardProps {
   todo: Todo;
+  updateTodo: (todo: Todo) => Promise<ResponseError | undefined>;
+  deleteTodo: (id: number) => Promise<ResponseError | undefined>;
 }
 
-export default function TodoCard({ todo }: TodoCardProps) {
+export default function TodoCard({
+  todo,
+  updateTodo,
+  deleteTodo,
+}: TodoCardProps) {
+  async function changeCompleteness() {
+    const error = await updateTodo({ ...todo, isComplete: !todo.isComplete });
+
+    if (error) {
+      alert(error.error);
+    }
+  }
   return (
     <div>
       <span data-testid="todo-description">{todo.description}</span>
       <span data-testid="todo-completeness">
         {todo.isComplete ? 'Done!' : 'Pending'}
       </span>
+      <button
+        data-testid="complete-button"
+        onClick={changeCompleteness}
+      >{`Mark ${todo.isComplete ? 'not' : ''} complete`}</button>
     </div>
   );
 }
--- frontend/test/TodoCard.spec.jsx
+++ frontend/test/TodoCard.spec.jsx
@@ -1,8 +1,24 @@
-import { describe, expect, test } from 'vitest';
-import { render, waitFor } from '@testing-library/react';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import { act, fireEvent, render, waitFor } from '@testing-library/react';
 import TodoCard from '../src/components/TodoCard.tsx';
 
+const updateTodo = vi.fn();
+// eslint-disable-next-line no-undef
+global.fetch = vi.fn();
+
+function createFetchResponse(data, isOk = true) {
+  return { json: () => new Promise((resolve) => resolve(data)), ok: isOk };
+}
+
+const BASE_URL = `${import.meta.env.VITE_BASE_URL}/todos`;
+
 describe('Todos list component', () => {
+  afterEach(() => {
+    updateTodo.mockReset();
+    // eslint-disable-next-line no-undef
+    global.fetch.mockReset();
+  });
+
   test("It renders a todo's details", () => {
     const mockTodo = { id: 1, description: 'First todo', isComplete: false };
     const { getByTestId } = render(<TodoCard todo={mockTodo} />);
@@ -10,13 +26,30 @@ describe('Todos list component', () => {
     expect(getByTestId('todo-completeness')).toBeInTheDocument();
   });
 
-  test.skip('It renders an empty list message if no todos are available', async () => {
-    const mockTodos = [];
-    const mockError = undefined;
-    const { getByTestId } = await waitFor(() =>
-      render(<TodosList todos={mockTodos} error={mockError} />)
+  test('It can be set as complete', async () => {
+    const mockTodo = { id: 1, description: 'First todo', isComplete: false };
+    const { getByTestId } = render(
+      <TodoCard todo={mockTodo} updateTodo={updateTodo} />
     );
-    expect(getByTestId('todos-list')).toHaveTextContent('No todos yet...');
+
+    expect(getByTestId('complete-button')).toHaveTextContent('Mark complete');
+
+    getByTestId('complete-button').click();
+    expect(updateTodo).toHaveBeenCalledWith({ ...mockTodo, isComplete: true });
+  });
+
+  test('It can be set as not complete', async () => {
+    const mockTodo = { id: 1, description: 'First todo', isComplete: true };
+    const { getByTestId } = render(
+      <TodoCard todo={mockTodo} updateTodo={updateTodo} />
+    );
+
+    expect(getByTestId('complete-button')).toHaveTextContent(
+      'Mark not complete'
+    );
+
+    getByTestId('complete-button').click();
+    expect(updateTodo).toHaveBeenCalledWith({ ...mockTodo, isComplete: false });
   });
 
   test.skip('It renders a list with todos', async () => {
